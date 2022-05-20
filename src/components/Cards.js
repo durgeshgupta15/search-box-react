c139cf3339e4fd81f4d76989f45998f852201c8f
durgesh_kumar_gupta <durgeshkumar.gupta@appzlogic.com>
2022-05-20 18:06:08 +0530

    margin added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ const Cards = (props) => {
         <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent:'center'}}>
             {
                 props.data.length > 0 && props.data.map(item => (
-                    <div className="card" key={item.id} style={{ width: '18rem', marginRight: '15px' }}>
+                    <div className="card" key={item.id} style={{ width: '18rem', margin: '15px' }}>
                         <img className="card-img-top" src={item.book_image} alt="Card image cap" />
                         <div className="card-body">
                             <h5 className="card-title">{item.name}</h5>
